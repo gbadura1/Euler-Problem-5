ecfe6507991f88620db6d7c900f1570bb750872e
Greg Badura <gbadura1@gmail.com>
2016-01-29 10:46:35 -0500

    attempting to use iterators - create iterator from array

--- problem 5/Source.cpp
+++ problem 5/Source.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 int main(){
-	some new stuff;
+	int div[10] = { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11 };
+	std::vector<int> my_vector(div, div + sizeof(div)/sizeof(int));
+
+
+
+
+
 	return 0;
 }
