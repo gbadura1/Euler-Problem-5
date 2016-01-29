70a0769ae7c64f54e59e5f5a10a41da8882b1e58
Greg Badura <gbadura1@gmail.com>
2016-01-29 11:34:32 -0500

    fix division by zero error

--- problem 5/Source.cpp
+++ problem 5/Source.cpp
@@ -8,19 +8,22 @@ bool divisible(int x, int y){
 }
 
 int main(){
-	int temp, max;
-	int div[10] = { 20, 19, 18, 17, 16, 15, 14, 13, 11 };
+	int temp, mult, min;
+	bool is_div;
+	int div[9] = { 20, 19, 18, 17, 16, 15, 14, 13, 11 };
 	std::vector<int> my_vector(div, div + sizeof(div)/sizeof(int));
-	for (int i = 19;; i * 19){
+	for (int i = 1;; i++){
+		mult = i * 19;
+		is_div = true;
 		for (std::vector<int>::iterator it = my_vector.begin(); it != my_vector.end(); ++it){
 			temp = *it;
-
+			if (!divisible(mult,temp)){ is_div = false; }
+		}
+		if (is_div == true){
+			min = mult;
+			break;
 		}
 	}
-
-
-
-
-
+	std::cout << "The minimum divisible by all from 20-1 is " << min;
 	return 0;
 }
