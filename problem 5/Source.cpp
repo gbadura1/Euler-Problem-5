8754fde5eb536a2f28d8c928653bfe33ba8190b4
Greg Badura <gbadura1@gmail.com>
2016-01-29 10:57:17 -0500

    check all multiples of 19 rather than 20

--- problem 5/Source.cpp
+++ problem 5/Source.cpp
@@ -2,10 +2,21 @@
 #include <string>
 #include <vector>
 
+bool divisible(int x, int y){
+	if (x%y == 0) { return true; }
+	else { return false; }
+}
 
 int main(){
-	int div[10] = { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11 };
+	int temp, max;
+	int div[10] = { 20, 19, 18, 17, 16, 15, 14, 13, 11 };
 	std::vector<int> my_vector(div, div + sizeof(div)/sizeof(int));
+	for (int i = 19;; i * 19){
+		for (std::vector<int>::iterator it = my_vector.begin(); it != my_vector.end(); ++it){
+			temp = *it;
+
+		}
+	}
 
 
 
