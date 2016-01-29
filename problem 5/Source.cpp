5fc71ef975726238c739addef2f891e584c75666
Greg Badura <gbadura1@gmail.com>
2016-01-29 11:41:48 -0500

    fixed, could have just used prime numbers

--- problem 5/Source.cpp
+++ problem 5/Source.cpp
@@ -12,7 +12,7 @@ int main(){
 	bool is_div;
 	int div[9] = { 20, 19, 18, 17, 16, 15, 14, 13, 11 };
 	std::vector<int> my_vector(div, div + sizeof(div)/sizeof(int));
-	for (int i = 1;; i++){
+	for (int i = 19*17*13*11;; i++){
 		mult = i * 19;
 		is_div = true;
 		for (std::vector<int>::iterator it = my_vector.begin(); it != my_vector.end(); ++it){
